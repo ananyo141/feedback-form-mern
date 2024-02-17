ee7e9d4c263bcb16c4812971fe98f49b2eb95e3e
Ananyobrata Pal <ananyo141@gmail.com>
2024-02-17 19:29:33 +0530

    feat(backend): scaffold endpoints

---
+++ backend/src/app.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import type { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (_req: Request, _res: Response) =>
+  _res.status(200).send({ message: "Hello World!" }),
+);
+
+app.post("/name", (req: Request, res: Response) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).send({ message: "Name is required" });
+  }
+  res.status(200).send({ message: `Hello ${name}!` });
+})
+
+const port = process.env.PORT || 3000;
+
+try {
+  // connect to database
+  // if (!process.env.MONGO_URI)
+  //   throw new Error("No connection string found in .env file");
+
+  // Server setup
+  app.listen(port, () => {
+    console.log(`Server listening on -> PORT ${port}`);
+  });
+} catch (error) {
+  console.error(error);
+}
